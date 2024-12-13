d2e8e9ffc7b826fbb9f8c4b45f49908b8d7846eb
BWhale1010 <bawool1010@daum.net>
2024-12-13 12:38:59 +0900

    int a 변수 추가 및 값 변경

--- WindowsFormsApp_20241213/Form1.cs
+++ WindowsFormsApp_20241213/Form1.cs
@@ -23,6 +23,7 @@ namespace WindowsFormsApp_20241213
                 텍스트 박스에 메시지 출력
              */
             textBox_print.Text = "이것은\r\n멀티라인\r\n텍스트박스\r\n인데요.";
+            int a = 1;
         }
 
 
