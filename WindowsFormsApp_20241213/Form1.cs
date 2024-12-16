1c18c2c9f3acab496afe49e3ed839e29ef8441f7
BWhale1010 <bawool1010@daum.net>
2024-12-16 09:31:30 +0900

    add hi

--- WindowsFormsApp_20241213/Form1.cs
+++ WindowsFormsApp_20241213/Form1.cs
@@ -24,6 +24,7 @@ namespace WindowsFormsApp_20241213
              */
             textBox_print.Text = "이것은\r\n멀티라인\r\n텍스트박스\r\n인데요.";
             int a = 1;
+            string msg = "hi";
         }
 
 
